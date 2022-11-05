1794412806156de3b4c10b297e62a259a73245a7
xiuyiqian <79441444+xiuyiqian@users.noreply.github.com>
2022-11-05 03:23:30 -0500

    Update link_state_node.py

--- routesim2/link_state_node.py
+++ routesim2/link_state_node.py
@@ -1,11 +1,14 @@
 from simulator.node import Node
 from graph import Graph, Edge
+import json
 
 class Link_State_Node(Node):
     def __init__(self, id):
         super().__init__(id)
         self.graph = Graph()
         self.reach = {}   # dest, cost
+        # for most recent routing msg for each edge
+        self.routing_msgs = {}
     # Return a string
     def __str__(self):
         return "Rewrite this function to define your node dump printout"
@@ -14,8 +17,24 @@ class Link_State_Node(Node):
     def link_has_been_updated(self, neighbor, latency):
         # latency =  -1 if delete a link
         if latency < 0:
-            self.graph.removeEdge(self.id,neighbor)
+            self.graph.removeEdge((self.id,neighbor))
+        else:
+            if self.graph.hasEdge((self.id,neighbor)):
+                self.graph.edges[(self.id,neighbor)].set_weight(latency)
+                self.graph.edges[(neighbor, self.id)].set_weight(latency)
+            else:
 
+                if neighbor not in self.graph.nodes:
+                    newNeighbor = Node(neighbor)
+                    newNeighbor.neighbors.append(self.id)
+                    self.graph.nodes[neighbor]=newNeighbor
+                else:
+                    newNeighbor = self.graph.nodes[neighbor]
+                newEdge = Edge(self.graph.nodes[self.id],newNeighbor,latency)
+                self.graph.addEdge(newEdge)
+
+                for i, old in self.routing_msgs.items():
+                    self.send_to_neighbor(neighbor, json.dumps(old))
 
     # Fill in this function
     def process_incoming_routing_message(self, m):
