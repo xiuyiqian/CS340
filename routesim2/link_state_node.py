0bca70c4c807015d9bcd4aa25dcc66e054f9c86a
Ying Zhang <zyalm1998@gmail.com>
2022-11-06 20:43:29 -0600

    run Dijkstra

--- routesim2/link_state_node.py
+++ routesim2/link_state_node.py
@@ -1,3 +1,5 @@
+import math
+
 from simulator.node import Node
 from graph import Graph, Edge
 import json
@@ -9,9 +11,51 @@ class Link_State_Node(Node):
         self.reach = {}   # dest, cost
         # for most recent routing msg for each edge
         self.routing_msgs = {}
+        self.sequence_number = 0
     # Return a string
     def __str__(self):
         return "Rewrite this function to define your node dump printout"
+
+    def find_min_dist_u(self):
+        # u := vertex in Q with minimum dist[u]
+        min_dist = math.inf
+        closet_node = None
+        for edge in self.graph.getEdges():
+            if edge.get_source() == self.id and edge.get_target() != self.id:
+                if edge.get_weight() < min_dist:
+                    closet_node = edge.get_target()
+        return closet_node, min_dist
+
+    def Dijkstra(self):
+        vertexes = self.graph.getNodes()
+        dist = {}
+        prev = {}
+        Q = {}
+        # construct the graphs
+        for vex in vertexes:
+            dist[vex] = math.inf
+            prev[vex] = None
+            Q[vex] = 0
+        dist[self.id] = 0
+
+        while len(Q) > 0:
+            # u := vertex in Q with minimum dist[u]
+            u, min_dist = self.find_min_dist_u()
+            if min_dist == math.inf:
+                print("no neighbors for node", self.id)
+                break
+            dist[u] = min_dist
+            Q.pop(u)
+            # try using u to make shorter path
+            for edge in self.graph.getEdges():
+                if edge.get_source() == u.id:
+                    # find neighbors of u
+                    v = edge.get_target()
+                    if min_dist + edge.get_weight() < dist[v]:
+                        dist[v] = min_dist + edge.get_weight()
+                        prev[v] = u
+
+        return dist, prev
 
     # Fill in this function
     def link_has_been_updated(self, neighbor, latency):
@@ -42,4 +86,11 @@ class Link_State_Node(Node):
 
     # Return a neighbor, -1 if no path to destination
     def get_next_hop(self, destination):
+        self.reach, prev = self.Dijkstra()
+        for key, val in prev:
+            if key == destination:
+                tmp_hop = key
+                while prev[tmp_hop] != self.id:
+                    tmp_hop = prev[tmp_hop]
+                return tmp_hop
         return -1
